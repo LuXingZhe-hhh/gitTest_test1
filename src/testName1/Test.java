193321ce59f0c9ad871b6f411a3e1987a39eeef8
LuXingZhe-hhh <2558970160@qq.com>
2021-02-01 21:16:06 +0800

    第一次测试通过IDEA提交项目到Git库

---
+++ src/testName1/Test.java
@@ -0,0 +1,8 @@
+package testName1;
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("PiChengDong");
+        System.out.println("in Package testName1");
+    }
+}
